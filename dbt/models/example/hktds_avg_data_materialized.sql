a6c280915720b40392b8432289ddb05e85894cd6
William Tsoi <williamtsoi@google.com>
2022-02-06 23:55:28 +0800

    changing partition key to as_of_date

--- dbt/models/example/hktds_avg_data_materialized.sql
+++ dbt/models/example/hktds_avg_data_materialized.sql
@@ -1,8 +1,8 @@
 {{ config(
     materialized='table',
     partition_by={
-      "field": "timestamp",
-      "data_type": "timestamp",
+      "field": "as_of_date",
+      "data_type": "date",
       "granularity": "day"
     }
 )}}
