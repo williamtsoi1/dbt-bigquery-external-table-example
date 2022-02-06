f06727a370224c9f48a85da86eabba1200f195f9
William Tsoi <williamtsoi@google.com>
2022-02-06 23:45:08 +0800

    initial commit

---
+++ dbt/models/example/hktds_avg_data_materialized.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "timestamp",
+      "data_type": "timestamp",
+      "granularity": "day"
+    }
+)}}
+
+select 
+    name, 
+    post_abbr, 
+    PARSE_DATE("%Y%m%d", as_of_date) as as_of_date, 
+    CAST(col1 AS INT64) as col1 , 
+    CAST(col2 AS FLOAT64) as col2, 
+    PARSE_TIMESTAMP("%F %T", col3) as timestamp
+from `dataset.hktds_avg_data`
